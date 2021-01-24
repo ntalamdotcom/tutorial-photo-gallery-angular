473c05e0f7a6612b020884bf57c7b6e8da0fe3a4
ntalam <61484617+ntalamdotcom@users.noreply.github.com>
2021-01-24 11:01:48 +1000

    explorer

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Filesystem, FilesystemDirectory } from '@capacitor/core';
 
 @Component({
   selector: 'app-tab3',
@@ -9,6 +10,34 @@ export class Tab3Page {
 
   constructor() {}
   public async showExplorer() {
-    console.log("fuel is bombing");
+    console.log('fuel is pumping engine...');
+    const fileName = `MyAwesomeFile.pdf`;
+    try {
+      Filesystem.writeFile({
+        path: fileName,
+        data: 'awesome possum',
+        directory: FilesystemDirectory.Documents
+        // encoding: FilesystemEncoding.UTF8
+      }).then((writeFileResult) => {
+        console.log('file created');
+        Filesystem.getUri({
+          directory: FilesystemDirectory.Documents,
+          path: fileName
+        }).then((getUriResult) => {
+          const path = getUriResult.uri;
+          /*this.fileOpener.open(path, 'application/pdf')
+            .then(() => {
+              // console.log('File is opened');
+            })
+            .catch(error => {
+              console.log('Error opening file', error);
+            });*/
+        }, (error) => {
+          console.log(error);
+        });
+      });
+    } catch (error) {
+      console.error('Unable to write file', error);
+    }
   }
 }
